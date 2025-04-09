433bb108b600327124954734f631073d896253ba
ka-sha <kasha1147@gmail.com>
2025-04-09 18:09:29 +0300

    SendParallelRequestsController implementation

--- kafka-message-sender/src/main/java/ru/vsavushkin/api/SendMessageToKafkaController.java
+++ kafka-message-sender/src/main/java/ru/vsavushkin/api/SendMessageToKafkaController.java
@@ -1,6 +1,16 @@
 package ru.vsavushkin.api;
 
 import org.openapitools.api.SendMessageToKafkaApi;
+import org.openapitools.model.SendToKafkaRequest;
+import org.openapitools.model.SendToKafkaResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SendMessageToKafkaController implements SendMessageToKafkaApi {
+
+    @Override
+    public ResponseEntity<SendToKafkaResponse> sendToKafka(SendToKafkaRequest sendToKafkaRequest) {
+        return SendMessageToKafkaApi.super.sendToKafka(sendToKafkaRequest);
+    }
 }
--- kafka-message-sender/src/main/java/ru/vsavushkin/api/SendParallelRequestsController.java
+++ kafka-message-sender/src/main/java/ru/vsavushkin/api/SendParallelRequestsController.java
@@ -2,5 +2,35 @@ package ru.vsavushkin.api;
 
 import org.openapitools.api.SendParallelRequestsApi;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
 public class SendParallelRequestsController implements SendParallelRequestsApi {
+
+    private final static RestTemplate restTemplate = new RestTemplate();
+    private final static ExecutorService executor = Executors.newFixedThreadPool(4);
+
+    @Override
+    public ResponseEntity<Void> sendParallelRequests(String body) {
+
+        final HttpEntity<Void> request = null; // ???
+        final String url = "localhost:20808/msg/incoming";
+        CompletableFuture<ResponseEntity<Void>> future = CompletableFuture.supplyAsync(
+                () -> restTemplate.exchange(url, HttpMethod.GET, request, Void.class), executor);
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
